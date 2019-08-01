0653c0c5761afa5a02e7e1cb5608af687afc1a9d
Felipe Parra <fparrav@icloud.com>
2019-08-01 15:15:08 -0400

    se modifica pom para trabajar con springbooot y deploy jar

--- src/main/java/cl/voxcom/demo/DemoApplication.java
+++ src/main/java/cl/voxcom/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer {
+public class DemoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
